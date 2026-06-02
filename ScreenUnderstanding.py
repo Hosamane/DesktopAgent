b456b33f06c6898a04facc0e53901a363569e0a5
hosamane <hosamanevs.is22@rvce.edu.in>
2026-06-03 01:59:43 +0530

    template based automation

--- ScreenUnderstanding.py
+++ ScreenUnderstanding.py
@@ -1,28 +1,76 @@
 
-
-from PIL import ImageGrab
 import cv2
+from PIL import ImageGrab
+import numpy as np
 import pyautogui
-import time 
+import time
+
+print("Taking screenshot...")
 pyautogui.hotkey('win', 'd')
 time.sleep(2)
 img = ImageGrab.grab()
+img.save("screen1.png")
 
-img.save("screen.png")
+print("Screenshot saved as screen1.png")
+screen = cv2.imread("screen1.png")
+template = cv2.imread("notepad_icon1.png")
 
-screen = cv2.imread("screen.png")
-template = cv2.imread("notepad_icon.png")
+if screen is None:
+    raise Exception("screen1.png not found")
 
-print(screen is None)
-print(template is None)
-print("Screenshot shape:", screen.shape)
-print("Template shape:", template.shape)
+if template is None:
+    raise Exception("notepad_icon1.png not found")
+
+
 result = cv2.matchTemplate(
-    screen,template,cv2.TM_CCOEFF_NORMED
+    screen,
+    template,
+    cv2.TM_CCOEFF_NORMED
+)
+
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+print(f"\nMatch Confidence: {max_val:.4f}")
+
+
+h, w = template.shape[:2]
+
+top_left = max_loc
+
+bottom_right = (
+    top_left[0] + w,
+    top_left[1] + h
+)
+
+center_x = top_left[0] + w // 2
+center_y = top_left[1] + h // 2
+
+
+cv2.rectangle(
+    screen,
+    top_left,
+    bottom_right,
+    (0, 255, 0),
+    3
+)
+
+cv2.circle(
+    screen,
+    (center_x, center_y),
+    5,
+    (0, 0, 255),
+    -1
 )
 
 
-_, max_val, _, max_loc = cv2.minMaxLoc(result)
+cv2.imwrite("detected.png", screen)
 
-print("Confidence:", max_val)
-print("Location:", max_loc)
+
+print("\nDetection Successful")
+print(f"Top Left     : {top_left}")
+print(f"Bottom Right : {bottom_right}")
+print(f"Center X     : {center_x}")
+print(f"Center Y     : {center_y}")
+pyautogui.doubleClick(center_x, center_y, duration=1)
+
+print("\ndetected.png saved")
